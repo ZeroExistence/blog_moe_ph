eaa15c4c8d29727a91e0e2e71ab4b7392dd9be86
ZeroExistence <tonydeguia009@gmail.com>
2020-12-11 15:12:30 +0800

    Initial commit.

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,21 @@
+<script>
+	import SideNav from '../components/SideNav.svelte';
+
+	export let open = false;
+</script>
+
+<style>
+	main {
+		position: relative;
+		max-width: 64em;
+		padding: 2em;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+</style>
+
+<SideNav {open}/>
+
+<main>
+	<slot></slot>
+</main>
---
+++ src/routes/index.svelte
@@ -0,0 +1,30 @@
+<script>
+	import successkid from 'images/successkid.jpg';
+</script>
+
+
+<svelte:head>
+	<title>Sapper project template</title>
+</svelte:head>
+
+<div class="grid gril-cols-1 items-center">
+<div>
+<h1>Great success!</h1>
+
+<figure>
+	<img alt="Success Kid" src="{successkid}">
+	<figcaption class="text-xl text-white">Have fun with Sapper!</figcaption>
+</figure>
+
+<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+</div>
+</div>
+
+<style lang="postcss">
+	h1 {
+		@apply text-3xl text-white;
+	}
+	p {
+		@apply text-xl text-white;
+	}
+</style>
