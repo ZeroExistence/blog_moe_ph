6811025fb7c5e804012e2af5726804a5127b68f6
ZeroExistence <tonydeguia009@gmail.com>
2020-12-13 20:01:26 +0800

    Move global css from _layout to tailwind.css due to unaapplied @apply during build.

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -12,33 +12,6 @@
 		margin: 0 auto;
 		box-sizing: border-box;
 	}
-	:global(body) {
-		@apply text-white;
-		background-color: #1F2937;
-	}
-	:global(p) {
-		@apply py-2;
-	}
-	:global(pre) {
-		@apply p-3 bg-gray-700 rounded overflow-x-auto;
-	}
-	:global(h1) {
-		@apply text-4xl;
-	}
-	:global(h2) {
-		@apply text-3xl py-3 border-b border-gray-700;
-	}
-	:global(h3) {
-		@apply text-2xl p-2;
-	}
-	:global(a) {
-		@apply underline;
-	}
-	:global(img) {
-		display: block;
-		margin: auto;
-		max-width: 100%;
-	}
 
 </style>
 
